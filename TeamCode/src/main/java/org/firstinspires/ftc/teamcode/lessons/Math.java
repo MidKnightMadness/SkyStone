e2161c1dc3045d7e13b0bb2296f0a824c9e1c8ee
jupiter.peng <pengjupiter5@gmail.com>
2019-09-03 16:33:08 -0700

    Wrote methods challenge

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/Math.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/lessons/Math.java
@@ -20,7 +20,7 @@ public class Math {
         (Its useful for telling if something is even or odd cause x % 2 is always 0 for evens and 1 for odds)
          */
 
-        int apples = 14;
+        int apples = 20;
         int oranges = 3;
 
         int applesAndOranges = apples + oranges;
