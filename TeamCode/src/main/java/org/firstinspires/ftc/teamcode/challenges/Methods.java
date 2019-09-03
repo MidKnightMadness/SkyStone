871d9ded2bb361e5ddac6e344ccaa76c2c5627fd
NyquilDreams <ian.halim@students.tka.org>
2019-09-03 16:48:46 -0700

    yeet?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/challenges/Methods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/challenges/Methods.java
@@ -5,11 +5,14 @@ package org.firstinspires.ftc.teamcode.challenges;
 public class Methods {
     public static void main(String[] args) {
         // Initialize two integer variable with two numbers
-
-
+        int yeet = 42;
+        int number = 99;
+        int tubular = methodMan(yeet, number);
+        System.out.print(yeet + number + "7" + tubular);
         // Create a method that will take three integer and return the double average of the three
+    }
 
-
+    private static int methodMan(int num, int num1) { return 2*((num + num1 + 7)/3); }
         // Use the average method to average the two variables and one constant value
 
 
@@ -17,5 +20,5 @@ public class Methods {
 
 
         // Try it again using println
-    }
+
 }
