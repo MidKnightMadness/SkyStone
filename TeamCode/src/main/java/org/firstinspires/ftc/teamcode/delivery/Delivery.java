072c9dc27b69db573434505992a5151ff07727bf
Jupiter Peng <pengjupiter5@gmail.com>
2019-10-08 16:47:49 -0700

    added folders for delivery, drive, foundationMover, grabber, intake added abstract classes for delivery, foundationMover, grabber, intake

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/delivery/Delivery.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.delivery;
+
+public abstract class Delivery {
+    protected abstract void init();
+    protected abstract void setHeight(double blocks);
+    protected abstract void setDepth(double inches);
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/foundationMover/FoundationMover.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.foundationMover;
+
+public abstract class FoundationMover {
+    protected abstract void init();
+    protected abstract void extend();
+    protected abstract void retract();
+    protected abstract void grab();
+}
