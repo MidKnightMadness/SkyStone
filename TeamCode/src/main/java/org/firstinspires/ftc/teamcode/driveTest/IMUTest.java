3ab4fadc1427137ecc8c042a2ecd1f5248d0c042
yichun.li <yichun.li@students.tka.org>
2019-10-09 16:57:47 -0700

    imu testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTest/IMUTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.driveTest;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+
+@TeleOp
+public class IMUTest extends OpMode {
+    private BNO055IMU imu;
+    private double x;
+    private double y;
+    private double z;
+
+    @Override
+    public void init(){
+        BNO055IMU.Parameters parameter = new BNO055IMU.Parameters();
+        parameter.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameter);
+        imu.startAccelerationIntegration(new Position(), new Velocity(), 50);
+
+    }
+
+    @Override
+    public void loop(){
+        x = imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle;
+        y = imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).secondAngle;
+        z = imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+
+        telemetry.addData("x: ", x);
+        telemetry.addData("y: ", y);
+        telemetry.addData("z: ", z);
+
+    }
+
+}
