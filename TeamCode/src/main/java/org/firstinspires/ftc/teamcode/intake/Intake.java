7bbd1c472d51519125241be0910840e5821b0fde
yichun.li <yichun.li@students.tka.org>
2019-10-08 17:22:02 -0700

    abstract intake class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/Intake.java
@@ -1,4 +1,9 @@
 package org.firstinspires.ftc.teamcode.intake;
 
 public abstract class Intake {
+    protected abstract void init();
+    protected abstract void spin(double power);
+    protected abstract void rotate(double power);
+
+
 }
