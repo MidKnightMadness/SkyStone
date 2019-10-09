99c21abade50e08bb44258fb736e1bded5620b3f
Jupiter Peng <pengjupiter5@gmail.com>
2019-10-08 17:14:37 -0700

    modified Grabber Interface

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/grabber/Grabber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/grabber/Grabber.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.grabber;
 
 public abstract class Grabber {
+    protected abstract void init();
     protected abstract void grab();
     protected abstract void release();
+    protected abstract boolean isFinished();
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/intake/Intake.java
@@ -1,5 +1,4 @@
 package org.firstinspires.ftc.teamcode.intake;
 
 public abstract class Intake {
-
 }
