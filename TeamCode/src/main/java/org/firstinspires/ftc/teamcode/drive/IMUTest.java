3f7c41559f72f1b8ff462626975834dc9524ded5
Gregory Ling <0b0100011101101100@gmail.com>
2019-10-10 09:09:08 -0700

    Renamed driveTest package to drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/IMUTest.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.driveTest;
+package org.firstinspires.ftc.teamcode.drive;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
