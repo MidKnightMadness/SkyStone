172ec6f9405d7fd2ff1bb10616b281a146291237
ken.mori <ken.mori@students.tka.org>
2019-10-19 15:16:08 -0700

    Finished the layout for Autonomous, added some files in common, and will program later once everything is finished.
    EDIT: Changed some minor issues and added some notes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonoumous/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonoumous/Autonomous.java
@@ -11,15 +11,22 @@ public class Autonomous extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
     //*********NOTE**********/
         //This is an idea on how autonomous runs, may change later
-    //*********AUTONOMOUS START************/
+        //These measurements are not accurate, this is just the general layout on how autonomous is going to function
+    //*********AUTONOMOUS START************//
         //Position(x, y, -90)// Move the robot facing the stone
+        //MoveTo(Position Target)//Move the robot until the grabber is 20 inches away from the first stone
+        //*******NOTE**********//
+            // Ground level is set to 0
+        //Delivery.height(5)//Move the grabber 5 inches from the ground
+        //Delivery.depth(20)//Move the grabber 20 inches to the stone
+        //Delivery.height(4)//Move the grabber down an inch until it touches the block
         //Grabber.init()//Grab the stone
         //Position(x, y, 0)//Move the robot so its facing the side with the foundation
         //MoveTo(Position target)//Move the robot to the foundation(Specific Location)
         //Position(x, y, -90)//Robot is facing the foundation
         //********NOTE*********//
-            // The foundation height is set to 0)
-        //Delivery.setHeight(7)//Move the grabber up until is it 5 inches above the foundation
+            // The foundation height is set to 2)
+        //Delivery.setHeight(9)//Move the grabber up until is it 5 inches above the foundation
         //Delivery.setDepth(10)//Move the grabber until it is 8 inches from the robot
         //Grabber.release()//Release stone on the foundation
         //Delivery.setHeight(0)//Reset delivery system
@@ -33,10 +40,14 @@ public class Autonomous extends LinearOpMode {
         //Position(x, y, 180)// Robot is facing the opposite side
         //MoveTo(Position target)//Move the robot to the other side until the side is facing the block
         //Position(x, y, -90)//Robot is facing the block
-        //Grabber.init()//Grabs the block again
+        //MoveTo(Position target)// Move the robot backwards or forwards until the grabber is 20 inches away from the second block
+        //Delivery.setHeight(5)//Move grabber 5 inches above the ground
+        //Delivery.setDepth(20)//Move grabber 20 inches to the stone
+        //Delivery.setDepth(4)//Move the grabber down an inch until it touches the block
+        //Grabber.init()//Grabs the stone again
         //Position(x, y, 0)//Robot faces the wall with the foundation
         //MoveTo(Position Target)//Robot moves to foundation
-        //Delivery.setHeight(13)//Move the grabber up until it is 11 inches away from the foundation
+        //Delivery.setHeight(15)//Move the grabber up until it is 13 inches away from the foundation
         //Delivery.setDepth(10)//Move the grabber until it is 8 inches from the robot
         //Grabber.release()//Release stone onto the foundation
         //Delivery.setHeight(0)//Reset delivery system
