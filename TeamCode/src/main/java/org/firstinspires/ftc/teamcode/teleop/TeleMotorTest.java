19d168edf79d1be905fa628b5415e3fe6f2893d4
NyquilDreams <ian.halim@students.tka.org>
2019-10-19 13:40:30 -0700

    mec wheels v2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleMotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleMotorTest.java
@@ -8,7 +8,9 @@ public class TeleMotorTest extends TeleMotor {
 
     DcMotor motor1;
     DcMotor motor2;
-
+    DcMotor motor3;
+    DcMotor motor4;
+    public double POWER_CONSTANT = 0.1;
 
     @Override
     public void init() {
@@ -16,21 +18,36 @@ public class TeleMotorTest extends TeleMotor {
         motor1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         motor2 = hardwareMap.dcMotor.get("motor2");
         motor2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor3 = hardwareMap.dcMotor.get("motor3");
+        motor3.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor4 = hardwareMap.dcMotor.get("motor4");
+        motor4.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     @Override
     public void loop() {
-        if(gamepad1.left_stick_y > 0){
-            motor1.setPower(0.7);
-            motor2.setPower(-0.5);
+        if(gamepad1.dpad_left){
+            motor1.setPower(POWER_CONSTANT);
+            motor2.setPower(POWER_CONSTANT);
+            motor3.setPower(-POWER_CONSTANT);
+            motor4.setPower(-POWER_CONSTANT);
         }
-        if(gamepad1.left_stick_y < 0){
-            motor1.setPower(-0.7);
-            motor2.setPower(0.5);
+        if(gamepad1.dpad_right){
+            motor1.setPower(-POWER_CONSTANT);
+            motor2.setPower(-POWER_CONSTANT);
+            motor3.setPower(POWER_CONSTANT);
+            motor4.setPower(POWER_CONSTANT);
         }
-        if(gamepad1.left_stick_button){
-            motor1.setPower(0);
-            motor2.setPower(0);
+        if(gamepad1.dpad_down){
+            motor1.setPower(-POWER_CONSTANT);
+            motor2.setPower(POWER_CONSTANT);
+            motor3.setPower(-POWER_CONSTANT);
+            motor4.setPower(POWER_CONSTANT);
         }
+        if(gamepad1.dpad_up)
+            motor1.setPower(POWER_CONSTANT);
+            motor2.setPower(-POWER_CONSTANT);
+            motor3.setPower(POWER_CONSTANT);
+            motor4.setPower(-POWER_CONSTANT);
     }
 }
