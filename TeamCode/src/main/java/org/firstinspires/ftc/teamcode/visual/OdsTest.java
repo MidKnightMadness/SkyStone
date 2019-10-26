f0a2f35779276f0178a0c56c94f6638f050645e1
Jupiter Peng <pengjupiter5@gmail.com>
2019-10-26 16:22:13 -0700

    added OdsTest

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visual/OdsTest.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.visual;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+
+@TeleOp
+public class OdsTest extends OpMode {
+
+    private OpticalDistanceSensor ods;
+
+    @Override
+    public void init() {
+        ods = hardwareMap.opticalDistanceSensor.get("ods");
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("light",ods.getLightDetected());
+        telemetry.addData("rawlight",ods.getRawLightDetected());
+        telemetry.addData("rawlightmax",ods.getRawLightDetectedMax());
+    }
+}
