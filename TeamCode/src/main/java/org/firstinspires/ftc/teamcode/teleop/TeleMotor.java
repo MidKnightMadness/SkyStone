839e981e215bdbbef27afd68fdbbb3cb4bbeb5ce
michael.zhang <michaeltzhang2020@gmail.com>
2019-10-26 13:46:53 -0700

    MZ TeleOP Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleMotor.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode.teleop;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.teamcode.common.AssemblyManager.Implementation;
+
+
+@Implementation(TeleOpDriveMZ.class)
+
+public abstract class TeleMotor extends OpMode {
+
+}
