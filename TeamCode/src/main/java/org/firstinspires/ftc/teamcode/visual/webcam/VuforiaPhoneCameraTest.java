8df135145e055963074558d948e24575371d3db8
Jupiter Peng <pengjupiter5@gmail.com>
2019-11-05 17:03:45 -0800

    fixed the gap detection between skystones and added variables for finding the time it takes to process a frame

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visual/webcam/VuforiaPhoneCameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/visual/webcam/VuforiaPhoneCameraTest.java
@@ -13,6 +13,9 @@ public class VuforiaPhoneCameraTest extends OpMode {
     private int frameCooldown = 0;  //allows loop to run multiple times between frames
     private int frameCooldownMax = 10;
     private String lines;   //lines for telemetry output
+    private long frameStart;    //for calculating the time for a frame to be processed
+    private float frameAvg;
+    private long frameCount;
 
     public VuforiaPhoneCameraTest()
     {
@@ -27,36 +30,43 @@ public class VuforiaPhoneCameraTest extends OpMode {
 
     @Override
     public void loop() {
-        telemetry.addLine(System.nanoTime() /1000000000.0 + "");
+        telemetry.addData("time",System.nanoTime() /1000000000.0 + "");
         if(frameCooldown > 0)
             frameCooldown--;
         else
         {
+            //start counting time
+            frameStart = System.nanoTime();
+
             frameCooldown = frameCooldownMax;
             Bitmap currentFrame = cameraManager.getCurrentFrame().copy(Bitmap.Config.RGB_565,true);
-            Bitmap scaledFrame = Bitmap.createScaledBitmap(currentFrame, 200, 100, false);
+            Bitmap scaledFrame = Bitmap.createScaledBitmap(currentFrame,200,50,false);
 
             //center pixel
             double[] hsv = new double[3];
             VuforiaPhoneCameraManager.colorToHSV(currentFrame.getPixel(currentFrame.getWidth() /2, currentFrame.getHeight() /2), hsv);
-            lines = "h:" + hsv[0] + ", s:" + hsv[1] + ", v:" + hsv[2] + "\n";
+            lines = "h: " + hsv[0] + ", s: " + hsv[1] + ", v: " + hsv[2] + "\n";
 
             //highlight the spaces between pixels to red
             int prevGreenX = 0;
             //highlights yellow pixels to green
-            for (int x = 0; x < scaledFrame.getWidth(); x++)
-                for (int y = 0; y < scaledFrame.getHeight(); y++)
+            for (int y = 0; y < scaledFrame.getHeight(); y++)
+                for (int x = 0; x < scaledFrame.getWidth(); x++)
                 {
                     VuforiaPhoneCameraManager.colorToHSV(scaledFrame.getPixel(x, y), hsv);
-                    if(Math.abs(hsv[0] - 43) < 7 && hsv[1] > 0.5)
+                    //33 to 47
+                    if(Math.abs(hsv[0] - 40) < 7 && hsv[1] > 0.5)
                     {
                         //set pixel to green
-                        //scaledFrame.setPixel(x, y, 0x00FF00);
+                        scaledFrame.setPixel(x, y, 0x00FF00);
 
-                        //find the distance to the previous green pixel
-                        if(Math.abs(x - prevGreenX) <= 5 && Math.abs(x - prevGreenX) >= 1) {
-                            scaledFrame.setPixel(x, y, 0x0000FF);
-                            telemetry.addLine("x:"+(x-1)+", y:"+y);
+                        //find the distance to the previous green pixel and set red
+                        if(Math.abs(x - prevGreenX) <= 4 && Math.abs(x - prevGreenX) >= 2 && x >= 1) {
+                            scaledFrame.setPixel(x - 1, y, 0xFF0000);
+
+                            //for (int redX = prevGreenX + 1; redX < x; redX++) {
+                            //    scaledFrame.setPixel(redX, y, 0xFF0000);
+                            //}
                         }
 
                         prevGreenX = x;
@@ -64,11 +74,17 @@ public class VuforiaPhoneCameraTest extends OpMode {
                 }
 
             //original frame size
-            lines += "width:" + currentFrame.getWidth() + "\n";
-            lines += "height:" + currentFrame.getHeight() + "\n";
+            lines += "width: " + currentFrame.getWidth() + "\n";
+            lines += "height: " + currentFrame.getHeight() + "\n";
 
             //displays the modified bitmap
             cameraManager.updatePreviewBitmap(scaledFrame);
+
+            //end of counting time
+            lines += "time taken: " + (System.nanoTime() - frameStart) /1000000000.0 +"\n";
+            frameAvg += (System.nanoTime() - frameStart) /1000000000.0;
+            frameCount++;
+            lines += "time taken avg: " + frameAvg / frameCount +"\n";
         }
         telemetry.addLine(lines);
         telemetry.update();
