3f1b8b3151d07191f61e5ee7b6838655137fb93a
michael.zhang <michaeltzhang2020@gmail.com>
2019-11-09 13:17:34 -0800

    Greggo Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navigationvisual/ExampleGregoyTemp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navigationvisual/ExampleGregoyTemp.java
@@ -5,7 +5,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaSkyStone;
 public class ExampleGregoyTemp {
     public void init() {
         VuforiaSkyStone vuforia = new VuforiaSkyStone();
-        vuforia.initialize();
+//        vuforia.initialize();
         vuforia.activate();
 
         vuforia.trackPose("Red Perimeter 1").x
