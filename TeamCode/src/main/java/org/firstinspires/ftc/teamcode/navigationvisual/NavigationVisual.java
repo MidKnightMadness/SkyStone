1dac8d54ebc03ceafe550e4508858cc9420b3222
Albert Shu <albertshu1729@gmail.com>
2019-11-09 16:41:37 -0800

    fixing a merge issue

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/navigationvisual/NavigationVisual.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.navigationvisual;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class NavigationVisual extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
