2024b20f241af7d32682ff9642df631bc4c33201
Albert Shu <albertshu1729@gmail.com>
2019-11-13 15:46:33 -0800

    clamp and scissor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clamp/LeadClamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/clamp/LeadClamp.java
@@ -5,7 +5,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 public class LeadClamp extends Clamp {
 
     DcMotor motor;
-    public double CLAMP_POWER = 0.1;
+    private double CLAMP_POWER = 0.1;
+    private double CLAMP_MIN_ENC = -10000000;
+    private double CLAMP_MAX_ENC = 10000000;
+    private double pos = 0;
 
 
     @Override
@@ -28,14 +31,16 @@ public class LeadClamp extends Clamp {
 
     @Override
     public void loop() {
-        if(gamepad1.left_trigger>0){
+        pos = motor.getCurrentPosition();
+
+        if(gamepad1.left_trigger>0 && pos < CLAMP_MAX_ENC){
             motor.setPower(CLAMP_POWER);
-        } else if (gamepad1.right_trigger>0){
+        } else if (gamepad1.right_trigger>0 && pos > CLAMP_MIN_ENC){
             motor.setPower(-CLAMP_POWER);
         } else {
             motor.setPower(0);
         }
-        telemetry.addLine(String.valueOf(motor.getCurrentPosition()));
+        telemetry.addLine(String.valueOf(pos));
         telemetry.update();
 
 
