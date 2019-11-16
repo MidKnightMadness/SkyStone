4d8e5c82a6446a8aabff51806307226ce85b66c5
michael.zhang <michaeltzhang2020@gmail.com>
2019-11-16 12:54:33 -0800

    MZ Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mover/Mover.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mover/Mover.java
@@ -3,5 +3,13 @@ package org.firstinspires.ftc.teamcode.mover;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 public abstract class Mover extends OpMode {
+    @Override
+    public void init() {
 
+    }
+
+    @Override
+    public void loop() {
+
+    }
 }
